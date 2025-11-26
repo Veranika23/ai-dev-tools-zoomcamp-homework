1e380d5e625397c1f11e8daf4f58d0bacb5fd34a
Veranika23 <89349282+Veranika23@users.noreply.github.com>
2025-11-26 09:07:56 +0000

    Q4

--- 01-ai-dev-tools-hw/todo_project/todos/views.py
+++ 01-ai-dev-tools-hw/todo_project/todos/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .models import Todo
+from .forms import TodoForm 
 
-# Create your views here.
+def todo_list(request):
+    todos = Todo.objects.all()
+    return render(request, 'todos/todo_list.html', {'todos': todos})
+
+
+def todo_create(request):
+    if request.method == 'POST':
+        form = TodoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('todo_list')
+    else:
+        form = TodoForm()
+    return render(request, 'todos/todo_form.html', {'form': form})
+
